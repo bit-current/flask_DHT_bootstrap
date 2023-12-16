4124a94903d834529c080dea13854f013d9d0f72
mekaneeky <alizawahry1@gmail.com>
2023-12-17 01:56:20 +0300

    initial dht helper

---
+++ app.py
@@ -0,0 +1,44 @@
+# Import the Flask module
+import argparse
+from dht import DHTSingleton
+from flask import Flask
+import requests
+
+def get_public_ip():
+    try:
+        response = requests.get('https://api.ipify.org')
+        return response.text
+    except requests.RequestException:
+        raise RuntimeError("Unable to get IP")
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def return_dht():
+    dht_object = DHTSingleton(app.dht_serve_port)
+    ip, port, p2p_key = dht_object.dht.get_visible_maddrs()[0].values()
+
+    try: 
+        getattr(app,'public_ip')
+    except AttributeError: #to set the public IP for the first time
+        app.public_ip = get_public_ip()
+    multiaddr = f"/ipv4/{app.public_ip}/tcp/{app.dht_serve_port}/p2p/{p2p_key}"
+    return multiaddr
+
+# Check if the script is run directly (not imported)
+if __name__ == '__main__':
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description='Flask Server Configuration')
+    parser.add_argument('--flask_serve_port', type=int, default=5000, help='Port to run the Flask server on')
+    # Custom placeholders for arguments
+    parser.add_argument('--dht_serve_port', type=int, default=5001, help='Port to run the DHT server on')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    app.dht_serve_port = args.dht_serve_port
+
+    # Run the Flask app on the specified port
+    app.run(debug=True, port=args.flask_serve_port)
---
+++ dht.py
@@ -0,0 +1,24 @@
+from hivemind import DHT
+import requests
+
+class DHTSingleton:
+    _instance = None
+
+
+    @staticmethod
+    def get_public_ip():
+        try:
+            response = requests.get('https://api.ipify.org')
+            return response.text
+        except requests.RequestException:
+            raise RuntimeError("Unable to get IP")
+    
+    def __new__(cls, dht_port):
+        if cls._instance is None: #TODO allow for port change without restart by checking for new port number
+            cls._instance = super(DHTSingleton, cls).__new__(cls)
+            cls._instance.dht = DHT(
+                host_maddrs=[f"/ip4/0.0.0.0/tcp/{dht_port}"],
+                start=True)  # Example data store
+        return cls._instance
+
+  
